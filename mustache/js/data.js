d9a3cea626b35c143be4688252a11739cc78ca79
Ed Wheeler <ewheeler3@gmail.com>
2014-07-19 12:29:56 -0700

    test using mustache.js templates

---
+++ mustache/js/data.js
@@ -0,0 +1,39 @@
+{"year" : {
+
+	"1996" : {
+		
+		"song" : [
+			
+			{
+				"title" : "Macarena (Bayside Boys Mix)",
+				"artist" : "Los del Rio",
+				"video" : "XiBYM6g8Tck"
+			},			
+			{
+				"title" : "Watcha Want",
+				"artist" : "Beastie Boys",
+				"video" : "ru3gH27Fn6E"
+			}
+		
+		]	
+	},
+	
+	"1997" : {
+		
+		"song" : [
+			
+			{
+				"title" : "Zig ah Zig",
+				"artist" : "Spice Girls",
+				"video" : "XiBYM6g8Tck"
+			},			
+			{
+				"title" : "Brain on Ska",
+				"artist" : "Mustard Plug",
+				"video" : "ru3gH27Fn6E"
+			}
+		
+		]	
+	}
+	
+}}	
